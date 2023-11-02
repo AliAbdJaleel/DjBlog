58907bb58726990af1c64b3a85910705a4d45f89
AliAbdJaleel <aliabdjaleel85@gmail.com>
2023-11-03 02:29:03 +0300

    add admin coustomization

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Posts
 
-admin.site.register(Posts)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','draft']
+    list_filter = ['draft']
+    search_fields = ['title']
+    
+    
+admin.site.register(Posts,ProductAdmin)

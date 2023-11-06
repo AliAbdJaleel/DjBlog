25d7949c914629fb13923c1bd6541c581db65e3d
AliAbdJaleel <aliabdjaleel85@gmail.com>
2023-11-06 08:50:56 +0300

    add image field to models

--- posts/models.py
+++ posts/models.py
@@ -15,10 +15,11 @@ class Posts(models.Model):
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)
     publish_date = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='post')
    # publish_date2 = models.DateTimeField(auto_now= True) # يتم انشاء عمود ياخذ تاريخ و وقت ادراج القيد دون ان يظهر للمستخدم
    # publish_date3 = models.DateTimeField(auto_now_add= True) # يتم انشاء عمود ياخذ تاريخ و وقت تحديث القيد دون ان يظهر للمستخدم
 
     tags = TaggableManager()
 
     def __str__(self):
-        return self.titleS
+        return self.title

64474ccd151859fb8c8105fb4c7c76ece745ccb0
AliAbdJaleel <aliabdjaleel85@gmail.com>
2023-11-13 02:24:58 +0300

    add post_detail

--- project/urls.py
+++ project/urls.py
@@ -25,6 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/',post_list),
+    path('posts/<int:post_id>',post_detail),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

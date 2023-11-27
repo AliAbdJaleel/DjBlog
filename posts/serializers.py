d8630cdb8db649f31c8bdd5caebacbcad9b451bd
AliAbdJaleel <aliabdjaleel85@gmail.com>
2023-11-28 01:24:19 +0300

    show relations as nested object

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,22 @@
 # form
 from rest_framework import serializers
+from django.contrib.auth.models import User # لغرض عرض تفاصيل المستخدم الذي قام بتحديث القيد او ادراجه
 from .models import Posts
+
+
+# هذه الكلاس لاظهار تفاصيل المستخدم او المؤلف في الجدول الرئيسي
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','email']
+
+
 
 
 class PostSerializer(serializers.ModelSerializer):
 
     # لاظهار قيم الحقول المرتبطة وليس الرقم
-    auther = serializers.StringRelatedField()
+    auther = UserSerializer()
     category = serializers.StringRelatedField()
     class Meta:
         model = Posts
